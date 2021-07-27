0390c7b5e7da3e42259436c585744d527037341f
bonfotieno <bonnyotieno9@gmail.com>
2021-07-27 23:36:37 +0300

    updated

---
+++ Web_Scraping_Google_Search_project.py
@@ -0,0 +1,18 @@
+# google.py
+import bs4
+import requests
+import sys
+import webbrowser
+
+print('Googling...')
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text)
+
+# Open a browser tab for each result.
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
